c65a2540359c840c5ac6f109d5649ad0037fbd3e
Shubham Yadav <hackspire.dev@gmail.com>
2025-08-12 19:02:48 +0530

    Calendar

---
+++ Basics/CalendarUsingSwitchCase.java
@@ -0,0 +1,27 @@
+import java.util.* ;
+public class CalendarUsingSwitchCase{
+    public static void calendar(int num){
+        switch(num){
+            case 1 : System.out.println("January");break;
+            case 2 : System.out.println("February"); break;
+            case 3 : System.out.println("March");break;
+            case 4 : System.out.println("April");break;
+            case 5 : System.out.println("May");break;
+            case 6 : System.out.println("June");break;
+            case 7 : System.out.println("July"); break;
+            case 8 : System.out.println("August");break;
+            case 9 : System.out.println("September");break;
+            case 10 : System.out.println("October");break;
+            case 11 : System.out.println("November");break;
+            case 12 : System.out.println("December"); break;
+            default : System.out.println("Enter correct number between (1-12)");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number :");
+        int n = sc.nextInt();
+        sc.close();
+        calendar(n);
+    }
+}
