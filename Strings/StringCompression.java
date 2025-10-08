7ebc7eab15c4d20799fc48f40d861e2f48629728
ShubhamYadav <ydvshubham.dev@gmail.com>
2025-10-08 22:30:05 +0530

    String Compression Problem

---
+++ Strings/StringCompression.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class StringCompression {
+    public static String compressString(String str) {
+        String newStr="";
+        for (int i = 0; i < str.length(); i++) {
+            Integer count = 1;
+            newStr+=str.charAt(i);
+            while (i<str.length()-1 && str.charAt(i) == str.charAt(i + 1)){
+                count++;
+                i++;
+            }
+            if (count > 1) {
+                newStr+=count.toString();
+            }
+        }
+        return newStr;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String str = sc.nextLine();
+        String output = compressString(str);
+        System.out.println("Compressed String is: " + output);
+        sc.close();
+    }
+}
